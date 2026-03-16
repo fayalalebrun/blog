330ca0a61d684bfcd92743b9f733092dc4658aa9
Francisco Ayala Le Brun <2482295+fayalalebrun@users.noreply.github.com>
2026-03-16 18:58:59 +0100

    Add RSS feed generation with ox-rss

--- build-site.el
+++ build-site.el
@@ -1,5 +1,8 @@
 ;; Set the package installation directory so that packages aren't stored in the
 ;; ~/.emacs.d/elpa path.
+(require 'cl-lib)
+(require 'org)
+(require 'ox-publish)
 (require 'package)
 (setq package-user-dir (expand-file-name "./.packages"))
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
@@ -12,9 +15,10 @@
 
 ;; Install dependencies
 (package-install 'htmlize)
+(package-install 'ox-rss)
 
 ;; Load the publishing system
-(require 'ox-publish)
+(require 'ox-rss)
 
 ;; Disable timestamp caching (needed for sandboxed builds like nix)
 (setq org-publish-use-timestamps-flag nil)
@@ -24,6 +28,127 @@
 (setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil) ;; Use our own styles
+
+(defun getenv-default (name fallback)
+  (let ((value (getenv name)))
+    (if (and value (not (string-empty-p value))) value fallback)))
+
+(defun ensure-trailing-slash (value)
+  (if (string-suffix-p "/" value) value (concat value "/")))
+
+(defconst site-url
+  (ensure-trailing-slash (getenv-default "SITE_URL" "https://noquiche.fyi")))
+
+(defconst site-title
+  (getenv-default "SITE_TITLE" "Blog"))
+
+(defconst site-description
+  (getenv-default "SITE_DESCRIPTION" "Posts from the blog."))
+
+(defconst site-author
+  (getenv-default "SITE_AUTHOR" site-title))
+
+(defconst site-email
+  (getenv-default "SITE_EMAIL" "noreply@example.com"))
+
+(defun content-file-to-url (file)
+  (concat site-url
+          (file-name-sans-extension
+           (file-relative-name file (expand-file-name "./content")))
+          ".html"))
+
+(defun org-file-keyword (keyword)
+  (car (cdr (assoc-string keyword (org-collect-keywords (list keyword)) t))))
+
+(defun fallback-post-title (file)
+  (mapconcat (lambda (word)
+               (if (<= (length word) 3)
+                   (upcase word)
+                 (capitalize word)))
+             (split-string (file-name-base file) "[-_]" t)
+             " "))
+
+(defun summary-candidate-p (text)
+  (let ((plain-text (replace-regexp-in-string
+                     "\\[\\[[^]]+\\]\\[\\([^]]+\\)\\]\\]"
+                     "\\1"
+                     text)))
+    (and (> (length plain-text) 40)
+         (not (string-prefix-p "[[" text))
+         (not (string-prefix-p "#+" text))
+         (not (string-prefix-p "IMAGE " plain-text))
+         (not (string-prefix-p "SCREENSHOT:" plain-text))
+         (not (string-prefix-p "Play " plain-text)))))
+
+(defun extract-post-summary ()
+  (let ((ast (org-element-parse-buffer)))
+    (catch 'summary
+      (org-element-map ast 'paragraph
+        (lambda (paragraph)
+          (let ((text (string-trim
+                       (buffer-substring-no-properties
+                        (org-element-property :begin paragraph)
+                        (org-element-property :end paragraph)))))
+            (when (summary-candidate-p text)
+              (throw 'summary text)))))
+      "Read the full post.")))
+
+(defun collect-post-metadata (file)
+  (with-temp-buffer
+    (insert-file-contents file)
+    (org-mode)
+    (let* ((title (or (org-file-keyword "TITLE")
+                      (fallback-post-title file)))
+           (date (or (org-file-keyword "DATE")
+                     (format-time-string
+                      "<%Y-%m-%d>"
+                      (file-attribute-modification-time (file-attributes file)))))
+           (summary (extract-post-summary)))
+      (list :file file
+            :title title
+            :date date
+            :summary summary
+            :url (content-file-to-url file)
+            :time (org-time-string-to-time date)))))
+
+(defun format-feed-entry (post)
+  (format (concat "* %s\n"
+                  ":PROPERTIES:\n"
+                  ":PUBDATE: %s\n"
+                  ":RSS_PERMALINK: %s\n"
+                  ":END:\n\n"
+                  "%s\n\n"
+                  "[[%s][Read the full post]]\n")
+          (plist-get post :title)
+          (plist-get post :date)
+          (substring (plist-get post :url) (length site-url))
+          (plist-get post :summary)
+          (plist-get post :url)))
+
+(defun generate-rss-source-file ()
+  (let* ((content-directory (expand-file-name "./content"))
+         (generated-directory (expand-file-name "./.generated"))
+         (feed-source (expand-file-name "feed.org" generated-directory))
+         (posts (sort
+                 (mapcar #'collect-post-metadata
+                         (cl-remove-if
+                          (lambda (file)
+                            (member (file-name-nondirectory file)
+                                    '("index.org")))
+                          (directory-files-recursively content-directory "\\.org$")))
+                 (lambda (left right)
+                   (time-less-p (plist-get right :time)
+                                (plist-get left :time))))))
+    (make-directory generated-directory t)
+    (with-temp-file feed-source
+      (insert (format "#+TITLE: %s\n" site-title))
+      (insert (format "#+DESCRIPTION: %s\n" site-description))
+      (insert (format "#+HTML_LINK_HOME: %s\n" site-url))
+      (insert (format "#+RSS_FEED_URL: %sfeed.xml\n" site-url))
+      (insert "#+OPTIONS: toc:nil num:nil\n\n")
+      (dolist (post posts)
+        (insert (format-feed-entry post))
+        (insert "\n")))))
 
 
 (defun sitemap-entry (entry style project)
@@ -71,9 +196,28 @@
 	     :base-extension "png\\|jpg\\|svg\\|css"
 	     :publishing-directory "./public/assets"
 	     :publishing-function 'org-publish-attachment
+	)
+       (list "org-site:rss"
+	     :base-directory "./.generated"
+	     :base-extension "org"
+	     :publishing-directory "./public"
+	     :publishing-function 'org-rss-publish-to-rss
+	     :exclude ".*"
+	     :include '("feed.org")
+	     :html-link-home site-url
+	     :html-link-use-abs-url t
+	     :rss-feed-url (concat site-url "feed.xml")
+	     :rss-extension "xml"
+	     :with-author t
+	     :author site-author
+	     :email site-email
+	     :section-numbers nil
+	     :table-of-contents nil
+	     :time-stamp-file nil
 	)))
 
 ;; Generate the site output
+(generate-rss-source-file)
 (org-publish-all t)
 
 (message "Build complete!")
